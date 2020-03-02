d54ccff0ace11a862545eb28e76282c5188b0dfa
kardespro <61694826+kardespro@users.noreply.github.com>
2020-03-02 18:02:07 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+res.render('index');
+});
+
+app.listen(8080);
+
+console.log('[kardespro]WebSiten Aktif');
+console.error('[kardespro] Bir şeyler Ters Gitti ');
